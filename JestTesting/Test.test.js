2ee285afb5d53901c703238a6de7bc5b4792fcfe
Matt Livesey <matt.livesey@roq.com>
2022-09-08 14:45:03 +0100

    edited the bug report file

--- JestTesting/Test.test.js
+++ JestTesting/Test.test.js
@@ -1,50 +1,75 @@
-const Browser = require("./browser");
+const Browser = require("./Browser");
 const browser = new Browser();
 const timeout = 20000;
 
-beforeEach(async () => {
-    browser.browserBuild();
-    await browser.browserNavigate('http://localhost:8080/');
-}, timeout);
+// test("initially nothing in password field", async() =>{
+// 	await expect(() => browser.getElement(password))
+// })
 
-afterEach(async () => {
-    await browser.browserExit();
-}, timeout);
+// test("initially nothing in username field", async() =>{
+// 	await expect(() => browser.getElement(username))
+// })
 
-test("initially nothing in password field", async () =>{
-	await expect(() => browser.getElement(username))
-});
+// test("Check there is a submit button", async () => {	
+// 	const element = await browser.getElementByCss();
+// 	const tagName = await element.getTagName();
+// 	expect(tagName).toBe();
+// })
 
-test("initially nothing in username field", async () =>{
-	await expect(() => browser.getElement(password))
-})
 
-test("Check there is a submit button", async () => {	
-	const element = await browser.getElementByCss();
-	const tagName = await element.getTagName();
-	expect(tagName).toBe();
-})
-
-test("Correct Password and Username", async () => {
-	const user = driver.findElement(by.id("username"));
-	const password = driver.findElement(by.id("password"));
-
+test("Sign in" , async () => {
+	const user = browser.findElement(username);
+	const passwrd = browser.findElement(password);
 	user.sendKeys("admin");
 	password.sendKeys("HelloWorld");
-
-    driver.findElement(by.id(Login())).click;
+	const SubButton = driver.findElement(By.id(login())).click();
+	
 })
 
-test("Empty fields login", async () => {
-    driver.findElement(by.id()).click;
-})
+// test("Add customer" , async () => {
+// 	const user = driver.findElement(by.id(username));
+// 	const password = driver.findElement(by.id(password));
+// 	user.sendKeys("admin");
+// 	password.sendKeys("HelloWorld");
+// 	const SubButton = driver.findElement(By.id(login())).click();
+// 	const NewCustButton = driver.findElement(By.id()).click();
+// 	const NewCustName = driver.findElement(by.id());
+// 	NewCustName.sendkeys("Delta Limited.");
+// 	const SaveButton = driver.findElement(By.id()).click();
+// })
 
-test("Special characters in username input", async () => {
-    const user = driver.findElement(by.id(""));
-    user.sendKeys("@!!!?")
-})
+// test("Create entity with name of special characters" , async() => {
+// 	const user = driver.findElement(by.id(username));
+// 	const password = driver.findElement(by.id(password));
+// 	user.sendKeys("admin");
+// 	password.sendKeys("HelloWorld");
+// 	const SubButton = driver.findElement(By.id()).click();
+// 	const NewCustButton = driver.findElement(By.id()).click();
+// 	const NewCustName = driver.findElement(by.id());
+// 	NewCustName.sendkeys("@!@@!@");
+// 	const SaveButton = driver.findElement(By.id()).click();
+// })
 
-test("Correct Username no password", async () => {
-	const user = driver.findElement(by.id(""));
-	user.sendKeys("admin");
-})
+// test("Add customer with same name as current customer" , async() => {
+// 	const user = driver.findElement(by.id(username));
+// 	const password = driver.findElement(by.id(password));
+// 	user.sendKeys("admin");
+// 	password.sendKeys("HelloWorld");
+// 	const SubButton = driver.findElement(By.id()).click();
+// 	const NewCustButton = driver.findElement(By.id()).click();
+// 	const NewCustName = driver.findElement(by.id());
+// 	NewCustName.sendkeys(SAMENAMEASOTHERCUSTOMER);
+// 	const SaveButton = driver.findElement(By.id()).click();
+// })
+
+// test("Add customer with extremely long name" , async() => {
+// 	const user = driver.findElement(by.id(username));
+// 	const password = driver.findElement(by.id(password));
+// 	user.sendKeys("admin");
+// 	password.sendKeys("HelloWorld");
+// 	const SubButton = driver.findElement(By.id()).click();
+// 	const NewCustButton = driver.findElement(By.id()).click();
+// 	const NewCustName = driver.findElement(by.id());
+// 	NewCustName.sendkeys("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+// 	const SaveButton = driver.findElement(By.id()).click();
+// })
