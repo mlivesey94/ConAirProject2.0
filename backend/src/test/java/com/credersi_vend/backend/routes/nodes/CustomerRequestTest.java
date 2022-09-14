be56cb1c004372a5621d9342f60875fe8e529b77
Matt Livesey <matt.livesey@roq.com>
2022-09-14 14:59:22 +0100

    Created test in backend for component

---
+++ backend/src/test/java/com/credersi_vend/backend/routes/nodes/CustomerRequestTest.java
@@ -0,0 +1,15 @@
+package com.credersi_vend.backend.routes.nodes;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class CustomerRequestTest {
+	
+	@Test
+	void createCustomer() {
+		assertTrue( 1==1 );
+	}
+}
